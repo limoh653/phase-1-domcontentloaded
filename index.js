0ef632ba4ebb77e137afac15d73cd0fe2d5a200c
limoh653 <limohesbon7@gmail.com>
2024-07-06 20:10:48 +0300

    DOM loaded

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    const paragraph = document.getElementById('text');
+    paragraph.innerHTML='<p>This is really cool!</p>';
+
+
+    console.log("The DOM has loaded");
+  });
